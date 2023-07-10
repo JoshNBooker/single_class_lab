1e7294b396eb8b6b7f3c2e37ea760b419919c0b3
Josh Booker <josh.booker2@gmail.com>
2023-07-10 14:31:27 +0100

    completed first task

---
+++ class.py
@@ -0,0 +1,12 @@
+from students import *
+
+student1 = students("josh","e65","python")
+
+students.talk(student1)
+
+student1.say_favourite_language()
+print(student1.say_favourite_language())
+
+team1 = sports_team("bluejays","Joe, jimmy, janey","Michael Smith")
+print(team1.name)
+
