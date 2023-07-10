9058f9a159102d8a94bced11e689ac95befb9fc2
Josh Booker <josh.booker2@gmail.com>
2023-07-10 14:54:47 +0100

    finished

--- class.py
+++ class.py
@@ -10,3 +10,12 @@ print(student1.say_favourite_language())
 team1 = sports_team("bluejays","Joe, jimmy, janey","Michael Smith")
 print(team1.name)
 
+team1.add_new_players("Jamie")
+print(team1.players)
+
+print(team1.has_player("Frankie"))
+
+team1.play_game("win")
+print(team1.points)
+
+team2 = sports_team("")
--- students.py
+++ students.py
@@ -17,8 +17,24 @@ class sports_team:
         self.name = name
         self.players = players
         self.coach = coach
+        self.points = 0
 
-    def add_new_players(self):
-        self.name += 
+    def add_new_players(self, new_player):
+        new_player = new_player.lower()
+        new_player = ", " + new_player
+        self.players += new_player
+
+    def has_player(self, player_check):
+        if player_check in self.players:
+            return True
+        else:
+            return False
+        
+    def play_game(self, result):
+        if result == "win":
+            points = True
+        if points == True:
+            self.points += 3
+    
 
         
